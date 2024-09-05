59df11f74b114faa11341991b42537461e391048
murukal <tutu@fantufantu.com>
2024-09-05 21:50:20 +0800

    feat: change layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -8,16 +8,21 @@ const Layout = () => {
   const theme = useTheme()
 
   return (
-    <div className='w-screen h-screen flex flex-col'>
-      <header className='py-2 px-2'>12321321</header>
+    <div
+      className='w-screen h-screen flex flex-row'
+      style={{
+        backgroundColor: theme.colors['surface-dim']
+      }}
+    >
+      <aside className='flex flex-col gap-4 p-4'>
+        <AddAlert size='large' />
+        <AutoDelete size='large' />
+      </aside>
 
-      <div className='flex-1 flex flex-row'>
-        <aside className='flex flex-col gap-2 px-2'>
-          <AddAlert size='large' />
-          <AutoDelete size='large' />
-        </aside>
+      <div className='flex-1 flex flex-col rounded-l-3xl' style={{ backgroundColor: theme.colors['surface'] }}>
+        <header className='py-2 px-2'>12321321</header>
 
-        <main className='flex-1 rounded-l-3xl' style={{ backgroundColor: theme.colors['surface-container'] }}>
+        <main className='flex-1'>
           <Suspense fallback={null}>
             <Outlet />
           </Suspense>
